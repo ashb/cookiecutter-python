34b67a011e3e8cc6abf75b9264aab39d1cc039af
Ash Berlin-Taylor <ash_github@firemirror.com>
2021-05-28 14:59:53 +0100

    Actually return lazy-loaded version

--- {{cookiecutter.project_name}}/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.module_name}}/__init__.py
@@ -3,15 +3,20 @@
 {{cookiecutter.description}}
 """
 
+
 def __getattr__(name):
     if name == "__version__":
         # Lazy load the version only if someone asks for it
-        ...
         try:
             from importlib import metadata
         except ImportError:
             import importlib_metadata as metadata
 
-        __version__ = metadata.version('{{cookiecutter.project_name}}')
+        try:
+            __version__ = metadata.version("{{cookiecutter.project_name}}")
+        except metadata.PackageNotFoundError:
+            __version__ = "0.0.0dev0"
+        globals()["__version__"] = __version__
+        return __version__
 
     raise AttributeError(f"module {__name__} has no attribute {name}")
